e9a79af38ad0fe0bba424f576f1829bfbe4f5331
NikLaaaa <164225777+NikLaaaa@users.noreply.github.com>
2025-11-20 23:11:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -714,7 +714,7 @@ function showLogs(chatId) {
     });
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Сервер работает на порту ${PORT}`);
 });
